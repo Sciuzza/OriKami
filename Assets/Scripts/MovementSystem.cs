6eb606cc934f3f12de02dc92c6b05b4f9cd3d08e
Cristiano Piromalli <juza9164@gmail.com>
2016-09-13 15:40:44 +0200

    Code System Structure Initialized

---
+++ Assets/Scripts/MovementSystem.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementSystem : PlayerCore {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
