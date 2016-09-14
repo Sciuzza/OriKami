23ccc3a82628aa729f22020ddfb72ec27bed872c
dbga_22 <dbga_22@dbga.local>
2016-09-14 18:32:13 +0200

    Implementazione labirinto

--- Assets/Scripts/TrapsManager.cs
+++ Assets/Scripts/TrapsManager.cs
@@ -1,15 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TrapsManager : MonoBehaviour {
+public class TrapsManager : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float sphereSpeed;
+    Transform targetTr;
+    public Transform A;
+    public Transform B;
+
+    void Awake()
+    {
+        targetTr = A;
+    }
+
+    void Update()
+    {
+
+        Vector3 distance = targetTr.position - this.transform.position;
+        Vector3 direction = (targetTr.position - this.transform.position).normalized;
+
+        transform.position = transform.position + direction * sphereSpeed * Time.deltaTime;
+
+        if (distance.magnitude < 5f)
+        {
+                    
+            if (targetTr == A)
+            {
+                targetTr = B;
+            }
+
+            else 
+            {
+                targetTr = A;
+            }
+        }
+
+    }
+ 
 }
