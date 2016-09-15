16539938fdeb4ca684c566fe661c83f68008cbb2
dbga_22 <dbga_22@dbga.local>
2016-09-15 17:55:03 +0200

    Aggiunta Rotazione alle Sfere / Crezione prefab trappola

--- Assets/Scripts/TrapsManager.cs
+++ Assets/Scripts/TrapsManager.cs
@@ -4,10 +4,14 @@ using System.Collections;
 public class TrapsManager : MonoBehaviour
 {
 
+
     public float sphereSpeed;
+    float sphereRotation = 5f;
     Transform targetTr;
     public Transform A;
     public Transform B;
+    Transform rotationSphere;
+    bool isDirectionRight = true;
 
     void Awake()
     {
@@ -19,23 +23,35 @@ public class TrapsManager : MonoBehaviour
 
         Vector3 distance = targetTr.position - this.transform.position;
         Vector3 direction = (targetTr.position - this.transform.position).normalized;
-
         transform.position = transform.position + direction * sphereSpeed * Time.deltaTime;
+
+        Vector3 newRotation = transform.localEulerAngles;
+
+        if (isDirectionRight == true)
+            newRotation.z -= sphereRotation * Time.deltaTime * sphereSpeed;
+
+        else if (isDirectionRight == false)
+            newRotation.z += sphereRotation * Time.deltaTime * sphereSpeed;
+
+        transform.localEulerAngles = newRotation;
 
         if (distance.magnitude < 5f)
         {
-                    
+
             if (targetTr == A)
             {
                 targetTr = B;
+                isDirectionRight = false;
             }
 
-            else 
+            else
             {
                 targetTr = A;
+                isDirectionRight = true;
+
             }
         }
 
     }
- 
+
 }
