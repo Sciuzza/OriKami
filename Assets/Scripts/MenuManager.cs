d1338cad5a2c71216d62dce966724098caf7a672
dbga_22 <dbga_22@dbga.local>
2016-09-19 18:05:28 +0200

    Menu Prototype

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+		
+    public void SceneLoader(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
