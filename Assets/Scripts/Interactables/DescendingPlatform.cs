d111f5da5d4d64ea6ad5accf1957c3c96edd721c
Riccardo <riccardocambiaghi@yahoo.it>
2016-10-06 02:50:13 +0200

    Floating Platforms ( need to fix rigidbody collisions)

--- Assets/Scripts/Interactables/DescendingPlatform.cs
+++ Assets/Scripts/Interactables/DescendingPlatform.cs
@@ -3,65 +3,15 @@ using System.Collections;
 
 public class DescendingPlatform : MonoBehaviour {
 
-    public Transform paddleDown;
-    public Transform paddleUp;
+    float weight = 350f;
 
-    Transform targetPaddle;
-
-    public bool isFalling = false;
-    public bool isDirectionDown = true;
-    public bool isOnPaddle = true;
-
-    public float downSpeed;
-      	
-    void Start()
+    void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        targetPaddle = paddleDown;
+        if (hit.rigidbody)
+        {
+            Debug.Log("Speriamo funzioni");
+            hit.rigidbody.AddForce(-hit.normal * weight);
+        }
+
     }
-
-
-   	void Update () {
-
-        //  FallingPlatform();
-
-        Vector3 distance = targetPaddle.position - this.transform.position;
-        Vector3 direction = (targetPaddle.position - this.transform.position).normalized;
-        transform.position = transform.position + direction * downSpeed * Time.deltaTime;
-
-        if (distance.magnitude < 0.1f)
-        {
-            if (targetPaddle == paddleDown)
-            {
-                targetPaddle = paddleUp;
-            }
-
-            else
-            {
-                targetPaddle = paddleDown;
-            }
-        }
-	}
-
-
-   /* void FallingPlatform()
-    {
-        if (isFalling)
-        {
-            
-            transform.position = new Vector3(transform.position.x, transform.position.y - downSpeed, transform.position.z);
-        }
-    }*/
-
-    void OnTriggerEnter(Collider coll)
-    {
-        Debug.Log("Scendo");
-        if (coll.gameObject.tag == "Player")
-        {
-
-            downSpeed += Time.deltaTime / 20;
-            
-           //  isFalling = true;
-
-        }
-    }
-}
+} 
