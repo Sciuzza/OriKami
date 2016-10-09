3c3ba061b9807462163d02a908299fb2f7e9ae29
Lollolink <lorenzopecce@gmail.com>
2016-10-09 21:22:06 +0200

    -Aggiunto prefab e logica Collectibles -Aggiunti Collectibles in scena Toybox DEF

---
+++ Assets/Collectibles.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectibles : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+
+    {
+        if (other.gameObject.CompareTag("Collectibles"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+}
+
