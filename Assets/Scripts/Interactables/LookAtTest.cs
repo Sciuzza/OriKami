f451f16b3ed2a3a51872e408f786991b0c669b05
Riccardo Cambiaghi <riccardocambiaghi@yahoo.it>
2016-10-25 15:45:36 +0200

    Puzzle 2.0 (Still Bugged)

---
+++ Assets/Scripts/Interactables/LookAtTest.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtTest : MonoBehaviour
+{
+
+    public Transform[] targetPosition = new Transform[3];
+    public Quaternion target;
+    public int positionIndex;
+    public bool isRotating = false, isOnButton = false;
+    
+
+    public void IndexController()
+    {
+              
+            isRotating = true;
+            isOnButton = true;
+
+            if (positionIndex <= 1)
+                positionIndex++;
+
+
+            else
+                positionIndex = 0;
+
+            target = Quaternion.LookRotation(targetPosition[positionIndex].forward, targetPosition[positionIndex].up);
+
+            StartCoroutine(movingWheel(target));
+        
+
+    }
+
+   public void OnTriggerController() {
+        isOnButton = false;
+    }
+        
+    
+
+    IEnumerator movingWheel(Quaternion target)
+    {
+        while (Quaternion.Angle(this.transform.rotation, target) > 0.1f)
+        {
+           this.transform.rotation = Quaternion.Slerp(this.transform.rotation, target, Time.deltaTime);
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+
+        //blueWheel.transform.rotation = target;
+        isRotating = false;
+    }
+
+}
---
+++ Assets/Scripts/Interactables/WheelActivator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelActivator : MonoBehaviour {
+
+    public bool isRotating = false, isOnButton = false;
+    public LookAtTest[] wheelLinker = new LookAtTest[2];
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && isRotating == false && isOnButton == false)
+        {
+           
+            wheelLinker[0].IndexController();
+            wheelLinker[1].IndexController();
+
+        }
+
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isOnButton = false;
+           wheelLinker[0].OnTriggerController();
+           wheelLinker[1].OnTriggerController();
+
+        }
+    }
+
+}
