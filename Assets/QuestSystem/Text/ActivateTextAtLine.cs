af8d22fe2d74053e7be3c0a9892cc1574198f9df
dbga_22 <dbga_22@dbga.local>
2016-11-02 17:22:57 +0100

    Dialogue System Deleted ObjAsset

---
+++ Assets/QuestSystem/Text/ActivateTextAtLine.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateTextAtLine : MonoBehaviour {
+
+    public TextAsset theText;
+
+    public int startLine;
+    public int endLine;
+
+    public TextBoxManager theTextBox;
+
+    public bool requireButtonPress;
+    private bool waitForPress;
+    public bool destroyWhenActivated;
+
+
+	void Start () {
+
+        theTextBox = FindObjectOfType<TextBoxManager>();
+
+	}
+	
+	void Update () {
+        if (waitForPress && Input.GetKeyDown(KeyCode.J))
+        {
+            theTextBox.ReloadScript(theText);
+            theTextBox.currentLine = startLine;
+            theTextBox.endAtLine = endLine;
+            theTextBox.EnableTextBox();
+
+            if (destroyWhenActivated)
+            {
+                Destroy(gameObject);
+            }
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag =="Player")
+        {
+            if (requireButtonPress)
+            {
+                waitForPress = true;
+                return;
+            }
+
+            theTextBox.ReloadScript(theText);
+            theTextBox.currentLine = startLine;
+            theTextBox.endAtLine = endLine;
+            theTextBox.EnableTextBox();
+
+            if (destroyWhenActivated)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag =="Player")
+        {
+            waitForPress = false;
+        }
+    }
+}
---
+++ Assets/QuestSystem/Text/TextBoxManager.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour
+{
+
+    public GameObject textBox;
+
+    public Text theText;
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    public int currentLine;
+    public int endAtLine;
+
+    public MoveCC player;
+
+    public bool isActive;
+    public bool stopPlayerMovement;
+
+    void Start()
+    {
+        player = FindObjectOfType<MoveCC>();
+
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+
+        if (endAtLine == 0)
+        {
+            endAtLine = textLines.Length - 1;
+        }
+
+        if (isActive)
+        {
+            EnableTextBox();
+        }
+        else
+        {
+            DisableTextBox();
+        }
+    }
+
+    void Update()
+    {
+        if (!isActive)
+        {
+            return;
+        }
+
+        theText.text = textLines[currentLine];
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            currentLine += 1;
+        }
+
+        if (currentLine > endAtLine)
+        {
+            DisableTextBox();
+        }
+    }
+
+    public void EnableTextBox()
+    {
+        textBox.SetActive(true);
+        isActive = true;
+    }
+
+    public void DisableTextBox()
+    {
+        textBox.SetActive(false);
+        isActive = false;
+    }
+
+    public void ReloadScript(TextAsset theText)
+    {
+        if (theText !=null)
+        {
+            textLines = new string[1];
+            textLines = (theText.text.Split('\n'));
+        }
+    }
+
+}
