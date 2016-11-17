b768483a82e33241ce2690e5329586d53208c839
dbga_22 <dbga_22@dbga.local>
2016-11-17 17:58:00 +0100

    Puzzle interaction system

--- Assets/Scripts/Interactables/Puzzles.cs
+++ Assets/Scripts/Interactables/Puzzles.cs
@@ -4,50 +4,83 @@ using System.Collections;
 
 public class Puzzles : MonoBehaviour {
 
-    public  float degree;
-
+    // bool per tutti i puzzle 
     public bool isPuzzle1;
     public bool isPuzzle2;
     public bool isPuzzle3;
     public bool isPuzzle4;
+    private bool keyHit = false;
 
-    public GameObject rotatingObject; 
+    public GameObject rotatingObject;
+    public GameObject generatedObject;
+    public GameObject leftFissure;
+    public GameObject rightFissure;
+    public GameObject platformGameobject;
 
+    private Vector3 startPosLeftDoor;
+    private Vector3 endPosLeftDoor;
 
+    private Vector3 startPosRightDoor;
+    private Vector3 endPosRightDoor;
 
-    // Use this for initialization
-    void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private float distance = 1f; // di quanto si muove la porta 
+    private float lerpTime = 5;
+    private float currentLerpTime = 0;
+     
+    
+    void Start()
+    {
+        startPosLeftDoor = leftFissure.transform.position;
+        endPosLeftDoor = leftFissure.transform.position + Vector3.forward * distance;
 
+        startPosRightDoor = rightFissure.transform.position;
+        endPosRightDoor = rightFissure.transform.position + Vector3.back * distance;
+    }
+
+    
+    IEnumerator DoorOpeningCO()
+    {
+        keyHit = true;
+        if (keyHit == true)
+        {
+            currentLerpTime += Time.deltaTime;
+            if (currentLerpTime >= lerpTime)
+            {
+                currentLerpTime = lerpTime;
+            }
+
+            float leftPerc = currentLerpTime / lerpTime;
+            float rightPerc = currentLerpTime / lerpTime;
+            leftFissure.transform.position = Vector3.Lerp(startPosLeftDoor, endPosLeftDoor, leftPerc);
+            rightFissure.transform.position = Vector3.Lerp(startPosRightDoor, endPosRightDoor, rightPerc);
+        }
+
+        yield return null;
+    }
 
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player" && isPuzzle4)
+        if (other.gameObject.tag == "Player" && isPuzzle2)
         {
-            StartCoroutine(MyCo());
+            Instantiate(generatedObject);
+            isPuzzle2 = false;
         }
+    
     }
 
-
-    IEnumerator MyCo()
+    void OnTriggerStay(Collider other)
     {
-        while (true)
+        if (other.gameObject.tag == "Player" && isPuzzle3)
         {
-            rotatingObject.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, degree), Time.deltaTime);
-            if (rotatingObject.transform.eulerAngles.y >= degree)
-            {
-                rotatingObject.transform.eulerAngles =new Vector3(0f, degree);
-                yield break;
-            }
-            else
-            yield return null;
+            StartCoroutine(DoorOpeningCO());
+            // leftFissure.transform.Translate(transform.right * 10 * Time.deltaTime);
+
         }
     }
+
+     
+
+
+   
 }
