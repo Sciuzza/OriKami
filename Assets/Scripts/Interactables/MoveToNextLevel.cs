1099d5a441702366c164cf3117764c6ddc517b6d
dbga_22 <dbga_22@dbga.local>
2016-11-22 16:31:00 +0100

    Script per trigger Passagio di livello !

---
+++ Assets/Scripts/Interactables/MoveToNextLevel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MoveToNextLevel : MonoBehaviour {
+
+    public void NextLevel()
+    {
+        //For completed levels !
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            NextLevel();
+        }
+    }
+}
---
+++ Assets/Scripts/Interactables/MoveToPreviousLevel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class MoveToPreviousLevel : MonoBehaviour {
+
+
+    public void PreviousLevel()
+    {
+        //For completed levels !
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+          PreviousLevel();
+        }
+    }
+}
