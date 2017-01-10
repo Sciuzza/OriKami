b58695b29c3dbaaf3f01a617ef842ed442fa544b
dbga_22 <dbga_22@dbga.local>
2017-01-10 18:08:25 +0100

    no message

---
+++ Assets/Scripts/SaveSystem/CheckPointTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPointTest : MonoBehaviour {
+
+    public SaveSystem save;
+    public bool saveGame, loadGame; 
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && saveGame)
+        {
+            Debug.Log("SONO DENTRO");
+            save.SaveState();
+        }
+
+        if (other.gameObject.tag == "Player" &&  loadGame)
+        {
+            Debug.Log("Sono Morto");
+            save.LoadState();
+        }
+    }
+
+
+}
