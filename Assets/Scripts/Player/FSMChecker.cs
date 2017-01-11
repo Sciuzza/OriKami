58d8ecaa96ed4f5290dc98c2d76e05511c5b111e
Cristiano Piromalli <juza9164@gmail.com>
2017-01-11 12:07:27 +0100

    Merge Fix phase 2

--- Assets/Scripts/Player/FSMChecker.cs
+++ Assets/Scripts/Player/FSMChecker.cs
@@ -122,11 +122,9 @@ public class FSMChecker : MonoBehaviour
     #endregion
 
     #region Private Variables
-<<<<<<< HEAD
+
     private PlayerInputs playerTemp;
-=======
-    
->>>>>>> Riki_Hub_Coglione
+
     [System.Serializable]
     public struct playerCState
     {
@@ -1609,13 +1607,10 @@ public class FSMChecker : MonoBehaviour
         if (deathZone.gameObject.tag == "Death")
         {
             Debug.Log("Morto");
-<<<<<<< HEAD
-            //save.LoadState();
-            // deathRequest.Invoke();
-=======
+
           
             deathRequest.Invoke();
->>>>>>> Riki_Hub_Coglione
+
 
         }
 
