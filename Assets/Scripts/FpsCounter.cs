c8991cd831828800d1917d0d775f15ee7ec9bdbf
dbga_28 <dbga_28@dbga.local>
2017-01-16 10:05:56 +0100

    Add FPS script, to use attach to a game object and check the Console

---
+++ Assets/Scripts/FpsCounter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FpsCounter : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(1.0f / Time.deltaTime);
+    }
+
+}
