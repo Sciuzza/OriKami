cdc128f5236cbc5df23af8ea77874a50201eaf8e
dbga_22 <dbga_22@dbga.local>
2017-02-01 17:38:06 +0100

    Logica per Collectible Contatore Hud per Collectible

--- Assets/Scripts/Player/Collectibles.cs
+++ Assets/Scripts/Player/Collectibles.cs
@@ -1,16 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
-public class Collectibles : MonoBehaviour {
+public class Collectibles : MonoBehaviour
+{
 
-	void OnTriggerEnter(Collider other)
 
-	{
-		if (other.gameObject.CompareTag("Collectibles"))
-		{
-			other.gameObject.SetActive(false);
-		}
-	}
+    int goldenCollectible = 0;
+    public Text countText;
+
+
+    void OnTriggerEnter(Collider other)
+
+    {
+        if (other.gameObject.tag == "Collectibles")
+        {
+            other.gameObject.SetActive(false);
+            goldenCollectible += 1;
+            countText.text = goldenCollectible.ToString();
+
+        }
+    }
+
 
 }
+
+
+
+
 
