c4aecd39752bf0b07dc38e92f3022803d58710fb
dbga_74 <dbga_74@dbga.local>
2017-02-03 12:33:09 +0100

    -Sostituiti rovi fissi e mobili e piattaforme a scomparsa -Creato script per far ruotare rovi mobili

---
+++ Assets/Rovorotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rovorotator : MonoBehaviour
+{
+
+    void Update()
+
+    {
+
+        transform.Rotate(new Vector3(0, 0, 90) * Time.deltaTime);
+
+    }
+    
+}
